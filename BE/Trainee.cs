03751388ae23608ac9e5598d2a2b4ab033b14e47
nachman shlomo levy <nalevy@g.jct.ac.il>
2018-12-12 20:21:18 +0200

    propertyes

--- BE/Trainee.cs
+++ BE/Trainee.cs
@@ -12,16 +12,16 @@ namespace BE
         public static  Configuration temp;
 
         int Trainee_id;
-        public string Trainee_name { get; set; }
-        public string Trainee_lname { get; set; }
-        public gender Trainee_gender { get; set; }
+        string Trainee_name;
+        string Trainee_lname;
+        gender Trainee_gender;
         int Trainee_tel;
-        public Adress Trainee_Adress { get; set; }
+        Adress Trainee_Adress;
         DateTime Trainee_Birthday;
-        public Car_type Trainee_ctype { get; set; }
-        public Transmission_type Trainee_transtype { get; set; }
-        public string School { get; set; }
-        public string My_tester { get; set; }
+        Car_type Trainee_ctype;
+        Transmission_type Trainee_transtype;
+        string School;
+        string My_tester;
         int num_class;
         Test my_last_test;
 
@@ -29,6 +29,36 @@ namespace BE
         {
             get { return Trainee_tel; }
             set { Trainee_tel = value; }
+        }
+        public string trainee_name
+        {
+            get { return Trainee_name; }
+            set { Trainee_name = value; }
+        }
+        public string trainee_lname
+        {
+            get { return Trainee_lname; }
+            set { Trainee_lname = value; }
+        }
+        public gender trainee_gender
+        {
+            get { return Trainee_gender; }
+            set { Trainee_gender = value; }
+        }
+        public Adress trainee_Adress
+        {
+            get { return Trainee_Adress; }
+            set { trainee_Adress = value; }
+        }
+        public Car_type trainee_ctype
+        {
+            get { return Trainee_ctype; }
+            set { Trainee_ctype = value; }
+        }
+        public Transmission_type trainee_transtype
+        {
+            get { return Trainee_transtype; }
+            set { Trainee_transtype = value; }
         }
         public Trainee()
         {
@@ -40,7 +70,16 @@ namespace BE
             get { return Trainee_id; }
             set { Trainee_id = value; }
         }
-
+        public string school
+        {
+            get { return School; }
+            set { School = value; }
+        }
+        public string my_tester
+        {
+            get { return My_tester; }
+            set { My_tester = value; }
+        }
         public DateTime traineebirthday
         {
             get { return Trainee_Birthday; }
