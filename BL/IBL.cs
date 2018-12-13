d7c74a9390429e35bf279d1924b58abc1d5d4171
levi isack eliezer <leviisac@g.jct.ac.il>
2018-12-13 20:35:31 +0200

    paam achrona

--- BL/IBL.cs
+++ BL/IBL.cs
@@ -5,7 +5,7 @@ using DAL;
 namespace BL
 {
     public interface IBL
-    {
+    {  //add functions
         void addtester(Tester x);
         void addtrainee(Trainee x);
         void addtest(Test x);
